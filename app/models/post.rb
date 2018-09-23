abe1d05eb124b19897462d83272a59be284d89a1
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 07:36:41 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,7 @@ end
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates :title, presence: true
+  validate(:title)
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
