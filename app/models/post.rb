5b4de365103aac0c4b16d38a9c388c495382f266
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 07:41:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 
 class TitleValidator < ActiveModel::Validator
   def validate(record)
-    unless record.include? "Won't Believe" || record.include? 'Secret' || record.include? 'Top [number]' || record.include? 'Guess'
+    unless record.title.include? "Won't Believe" || record.title.include? 'Secret' || record.title.include? 'Top [number]' || record.title.include? 'Guess'
       record.errors[:title] << 'Title is not clickbait-y enough.'
     end
   end
@@ -9,8 +9,7 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates :title, presence: true
-  validate(:title)
+  validates :title, presence: true, title: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
