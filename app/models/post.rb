c108ed032337bdda35bbb0e91fa915d5e1756a0f
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 07:31:22 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
+
+class TitleValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.name.starts_with? 'X'
+      record.errors[:name] << 'Need a name starting with X please!'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
