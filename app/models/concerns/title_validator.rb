c5bc9eaf7948fe7e0d6dd7cf7aaafbe5454e6d00
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 07:59:45 +0000

    automatically backed up by learn

--- app/models/concerns/title_validator.rb
+++ app/models/concerns/title_validator.rb
@@ -1,7 +1,11 @@
 class TitleValidator < ActiveModel::Validator
   def validate(record)
-    unless record[:title].include? "Won't Believe" || record[:title] == nil
-      record.errors[:title] << 'Title not valid.'
+    if record.title != nil
+      if !record.title.include? "Won't Believe"
+        record.errors[:title] << 'Title not valid.'
+      end
+    else
+      record.errors[:title] << 'Title is empty.'
     end
   end
 end
