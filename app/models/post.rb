ceef8d7bc3dd22fc97befe60d00883d80a2343d1
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 07:46:05 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,3 @@
-
-class TitleValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.title.include? "Won't Believe" || record.title.include? 'Secret' || record.title.include? 'Top [number]' || record.title.include? 'Guess'
-      record.errors[:title] << 'Title is not clickbait-y enough.'
-    end
-  end
-end
-
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates :title, presence: true, title: true
