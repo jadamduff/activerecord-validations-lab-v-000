f85ab09e7929c39065071779f60a435d96f51886
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 07:34:48 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
+  validates_with TitleValidator
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
